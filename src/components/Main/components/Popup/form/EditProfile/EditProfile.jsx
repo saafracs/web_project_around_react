c4824fdffe7b068d340b06a371b27936a8377619
Andres Rivas <andres.es.digital@gmail.com>
2025-10-19 07:34:19 -0500

    React 14

--- src/components/Main/components/Popup/form/EditProfile/EditProfile.jsx
+++ src/components/Main/components/Popup/form/EditProfile/EditProfile.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import CurrentUserContext from "../../../../../../../contexts/CurrentUserContext";
+import CurrentUserContext from "../../../../../../contexts/CurrentUserContext";
 
 export default function EditProfile() {
   const userContext = useContext(CurrentUserContext); // Obtiene el objeto currentUser
